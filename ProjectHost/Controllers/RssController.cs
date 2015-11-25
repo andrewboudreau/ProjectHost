4e624d7e5c3b75f1f40e1fcbdc2b39be426b9a2a
Andrew <andrewboudreau@gmail.com>
2015-11-24 20:14:11 -0600

    Adding skeleton for RSS

---
+++ ProjectHost/Controllers/RssController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectHost.Controllers
+{
+    public class RssController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request == null)
+            {
+                throw new NullReferenceException("Request cannot be null");
+            }
+
+            if (Request.Url == null)
+            {
+                throw new NullReferenceException("Request.Url cannot be null");
+            }
+
+            if (Request.ApplicationPath == null)
+            {
+                throw new NullReferenceException("Request.ApplicationPath cannot be null");
+            }
+
+            var baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/";
+            SyndicationFeed feed = new SyndicationFeed("ProjectHost", "ProjectHost Releases Feed", new Uri(baseUrl + "/RSS/"), "ProjectHostAll", DateTime.Now);
+
+            
+            return new 
+        }
+    }
+}
