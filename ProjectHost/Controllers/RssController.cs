3acdc1dfbb653c1ec856d5ec6dd399ea29037f31
andrewboudreau <andrewboudreau@gmail.com>
2015-11-24 22:19:22 -0600

    naive rss feed
    added simple rss feed of recent 50 releases

--- ProjectHost/Controllers/RssController.cs
+++ ProjectHost/Controllers/RssController.cs
@@ -1,46 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.ServiceModel.Syndication;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Xml;
+using ProjectHost.Models;
 
 namespace ProjectHost.Controllers
 {
     public class RssController : Controller
     {
-        public ActionResult Index()
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        public Uri BaseUri
         {
-            if (Request == null)
+            get
             {
-                throw new NullReferenceException("Request cannot be null");
+                if (Request == null)
+                {
+                    throw new NullReferenceException("Request cannot be null");
+                }
+
+                if (Request.Url == null)
+                {
+                    throw new NullReferenceException("Request.Url cannot be null");
+                }
+
+                // ReSharper disable once Html.PathError
+                return new Uri(Request.Url, Url.Content("~/rss"));
+            }
+        }
+
+        public async Task<RssActionResult> Index()
+        {
+            var releases = await db.Releases
+                .AsNoTracking()
+                .Include(r => r.Project)
+                .OrderByDescending(r => r.Id)
+                .Take(50)
+                .ToListAsync();
+            
+            var feed = new SyndicationFeed("ProjectHost", "ProjectHost Releases Feed", BaseUri, "ProjectHostAll", DateTime.Now);
+            var syndicationItems = new List<SyndicationItem>(50);
+
+            foreach (var release in releases)
+            {
+                var url = Url.Action("Download", "Releases", new { projectId = release.ProjectId, releaseId = release.Id });
+                if (url == null)
+                {
+                    throw new NullReferenceException("Release Download route has changed");
+                }
+
+                var title = $"{release.Project.Name} {release.Version}";
+                var content = $"Project:{release.Project.Name} {Environment.NewLine} Version:{release.Version} {Environment.NewLine} {release.Notes}";
+                var uriBuilder = new UriBuilder(BaseUri) { Path = url };
+                var item = new SyndicationItem(title, content, uriBuilder.Uri);
+
+                syndicationItems.Add(item);
             }
 
-            if (Request.Url == null)
+            feed.Items = syndicationItems;
+            return new RssActionResult() { Feed = feed };
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                throw new NullReferenceException("Request.Url cannot be null");
+                db.Dispose();
             }
-
-            if (Request.ApplicationPath == null)
-            {
-                throw new NullReferenceException("Request.ApplicationPath cannot be null");
-            }
-
-            var baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/";
-            SyndicationFeed feed = new SyndicationFeed("ProjectHost", "ProjectHost Releases Feed", new Uri(baseUrl + "/RSS/"), "ProjectHostAll", DateTime.Now);
-
-
-            return new RssActionResult(feed);
+            base.Dispose(disposing);
         }
 
         public class RssActionResult : ActionResult
         {
-            public RssActionResult(SyndicationFeed feed)
-            {
-                Feed = feed;
-            }
-
             public SyndicationFeed Feed { get; set; }
 
             public override void ExecuteResult(ControllerContext context)
